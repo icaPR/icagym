19debba0520964b50cdb816f9011e150dd34496d
Icaro Ramos <polosnramos@hotmail.com>
2023-12-04 03:27:53 -0300

    feat: screen SignUp

---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,46 @@
+import { Center, Image, VStack, Text, Heading, ScrollView } from "native-base";
+
+import LogoSVG from "@assets/logo.svg";
+import BackgroundIMG from "@assets/background.png";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+export function SignUp() {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+        <Image
+          source={BackgroundIMG}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
+        <Center my={24}>
+          <LogoSVG />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Crie sua conta
+          </Heading>
+          <Input placeholder="Nome" />
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry />
+          <Button title="Criar e acessar" />
+        </Center>
+
+        <Button title="Voltar para o login" variant="outline" mt={24} />
+      </VStack>
+    </ScrollView>
+  );
+}
