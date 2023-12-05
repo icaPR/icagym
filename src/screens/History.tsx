f90cc690ff3c47cfdd187ea6124de6ea7be600f9
Icaro Ramos <polosnramos@hotmail.com>
2023-12-05 07:44:45 -0300

    feat: screen History

---
+++ src/screens/History.tsx
@@ -0,0 +1,37 @@
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Heading, VStack, SectionList, Text } from "native-base";
+import { useState } from "react";
+
+export function History() {
+  const [excercises, setExcercises] = useState([
+    { title: "10.10.2023", data: ["supino", "halter"] },
+    { title: "11.10.2023", data: ["supino", "halter"] },
+  ]);
+
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+      <SectionList
+        sections={excercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={
+          excercises.length === 0 && { flex: 1, justifyContent: "center" }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrado ainda. {"\n"}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+      />
+    </VStack>
+  );
+}
