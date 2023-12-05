bf83c3674b8523115e30b93752197f82928f6b15
Icaro Ramos <polosnramos@hotmail.com>
2023-12-05 17:25:38 -0300

    feat: App

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { THEME } from "./src/theme";
+import { NativeBaseProvider, View } from "native-base";
+
+import { StatusBar } from "react-native";
+import { Loading } from "@components/Loading";
+import { Routes } from "./src/routes";
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
