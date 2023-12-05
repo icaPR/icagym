c31e9d6e3e5d618d1d96e9820b84efa4ebeaac91
Icaro Ramos <polosnramos@hotmail.com>
2023-12-05 08:42:32 -0300

    feat: screen Exercise

---
+++ src/screens/Exercise.tsx
@@ -0,0 +1,91 @@
+import {
+  HStack,
+  Heading,
+  Icon,
+  Text,
+  VStack,
+  Image,
+  Box,
+  ScrollView,
+} from "native-base";
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import { Button } from "@components/Button";
+
+import BodySvg from "../assets/body.svg";
+import SeriesSvg from "../assets/series.svg";
+import RepetitionsSvg from "../assets/repetitions.svg";
+export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function HandleGoBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={HandleGoBack}>
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+        </TouchableOpacity>
+        <HStack
+          justifyContent="space-between"
+          mt={4}
+          mb={8}
+          alignItems="center"
+        >
+          <Heading color="gray.100" fontSize="lg" flexShrink={1}>
+            Supino reto
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+            <Text color="gray.200" ml={1} textTransform="capitalize">
+              Peito
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+      <ScrollView>
+        <VStack p={8} mb={4}>
+          <Image
+            w="full"
+            h={80}
+            source={{
+              uri: "https://www.dicasdeacademia.com.br/wp-content/uploads/2023/06/supino-reto-768x421.webp",
+            }}
+            alt="Nome do exercício"
+            mb={3}
+            resizeMode="cover"
+            rounded="lg"
+          />
+          <Box bg="gray.600" rounded="md" pb={4} px={4}>
+            <HStack
+              alignItems="center"
+              justifyContent="space-around"
+              mb={6}
+              mt={5}
+            >
+              <HStack>
+                <SeriesSvg />
+                <Text color="gray.200" ml={2}>
+                  4 series
+                </Text>
+              </HStack>
+              <HStack>
+                <RepetitionsSvg />
+                <Text color="gray.200" ml={2}>
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+            <Button title="Marcar como realizado" />
+          </Box>
+        </VStack>
+      </ScrollView>
+    </VStack>
+  );
+}
