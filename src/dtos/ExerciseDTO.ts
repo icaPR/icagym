eafc4674fdd28c5e0a781b8d4d9c29f3b87e1569
Icaro Ramos <polosnramos@hotmail.com>
2023-12-10 16:46:35 -0300

    chore:  ExerciseDTO/HistoryDTO/ ExerciseDTO

---
+++ src/dtos/ExerciseDTO.ts
@@ -0,0 +1,12 @@
+export type ExerciseDTO = {
+  id: string;
+
+  name: string;
+  group: string;
+  thumb: string;
+
+  series: number;
+  repetitions: string;
+  demo: string;
+  update_at: string;
+};
